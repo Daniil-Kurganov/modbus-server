babce32705301c0142107fa1164b928a61613c04
Daniil Kurganov <dk30128@gmail.com>
2024-12-03 10:16:58 +0300

    Added trace log for connection

--- servetcp.go
+++ servetcp.go
@@ -20,7 +20,7 @@ func (s *Server) accept(listen net.Listener) error {
 			log.Printf("Unable to accept connections: %#v\n", err)
 			return err
 		}
-
+		log.Printf("New connection: type - %s, address - %s", conn.RemoteAddr().Network(), conn.RemoteAddr().String())
 		go func(conn net.Conn) {
 			defer conn.Close()
 
