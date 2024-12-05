07131411cb566de51f08c28f2fac7bcde48a9ab6
Daniil Kurganov <dk30128@gmail.com>
2024-12-05 15:47:55 +0300

    Implemented connection notification channel check

--- servetcp.go
+++ servetcp.go
@@ -22,7 +22,9 @@ func (s *Server) accept(listen net.Listener) error {
 			return err
 		}
 		log.Printf("New connection: type - %s, address - %s", conn.RemoteAddr().Network(), conn.RemoteAddr().String())
-		s.ConnectionChanel <- &conn
+		if _, ok := <-s.ConnectionChanel; ok {
+			s.ConnectionChanel <- &conn
+		}
 		go func(conn net.Conn) {
 			defer conn.Close()
 
